cb9bdd99e49cf45bb37e0dabaeaed796e576a8a2
Kagan Dikmen <kagandikmen@outlook.com>
2024-09-03 04:51:10 +0200

    make the sr demo generic

--- software/sr_demo/main.c
+++ software/sr_demo/main.c
@@ -10,6 +10,9 @@
 #include "platform.h"
 #include "uart.h"
 
+#define SOURCE_WIDTH 32
+#define SOURCE_HEIGHT 32
+
 static struct uart uart0;
 
 void exception_handler(uint32_t cause, void * epc, void * regbase)
@@ -23,11 +26,13 @@ int main()
     uart_initialize(&uart0, (volatile void *) PLATFORM_UART0_BASE);
 	uart_set_divisor(&uart0, uart_baud2divisor(115200, PLATFORM_SYSCLK_FREQ));
     
-    unsigned char original_image [4096] = {};
+    int num_source_pixels = SOURCE_WIDTH * SOURCE_HEIGHT;
 
-    unsigned char enhanced_image [16384] = {};
+    unsigned char original_image [num_source_pixels] = {};
 
-    for(int i = 0; i < 64 * 64; i++){
+    unsigned char enhanced_image [num_source_pixels*4] = {};
+
+    for(int i = 0; i < num_source_pixels; i++){
 		while(uart_rx_fifo_empty(&uart0));
 		*((volatile uint8_t*)(original_image + i)) = uart_rx(&uart0);
 	}
@@ -36,21 +41,23 @@ int main()
     asm volatile("add x29, x0, %[a]"::[a] "r" (enhanced_image):);
     
     
-    for(int i=0; i<64; i++){
+    for(int i=0; i<SOURCE_HEIGHT; i++){
         asm volatile("ctrst");
-        for(int j=0; j<16; j++) {
+        for(int j=0; j<(SOURCE_WIDTH>>2); j++) {
+            asm volatile("lw x30, 0(x28)");
+            asm volatile("add x31, x28, %[a]"::[a] "r" (SOURCE_WIDTH):);
             asm volatile(
-                        "lw x30, 0(x28)\n\t"
-                        "lw x31, 64(x28)\n\t"
+                        "lw x31, 0(x31)\n\t"
                         "enh x30, x31\n\t"
                         "lf0 0(x29)\n\t"
                         "lf1 4(x29)"
                         );
-            if(i != 63) // Corner case: last row
+            if(i != SOURCE_HEIGHT-1)    // Corner case: last row
             {
+                asm volatile("add x30, x29, %[a]"::[a] "r" (SOURCE_WIDTH*2):);
                 asm volatile(
-                            "lf2 128(x29)\n\t"
-                            "lf3 132(x29)"
+                            "lf2 0(x30)\n\t"
+                            "lf3 4(x30)"
                             );
             }
             asm volatile(
@@ -59,10 +66,10 @@ int main()
                         );
             
         }
-        asm volatile("addi x29, x29, 0x80");
+        asm volatile("add x29, x29, %[a]"::[a] "r" (SOURCE_WIDTH*2));
     }
 
-    uart_tx_array(&uart0, enhanced_image, 16384);
+    uart_tx_array(&uart0, enhanced_image, num_source_pixels*4);
     
     return 0;
 }
